ae2f6f8a3ff52a7ad788dbde7f80d77a2ee70be8
dzv <dzvgithub@gmail.com>
2023-11-11 12:05:29 +0900

    multi perceptron

--- perceptron.py
+++ perceptron.py
@@ -15,15 +15,49 @@ Y=torch.FloatTensor([[0],[1],[1],[0]]).to(device)
 linear= nn.Linear(2,1,bias=True)
 sigmoid=nn.Sigmoid()
 #nn.Sequential: run the functions first to end in turn.
-model=nn.Sequential(linear,sigmoid).to(device)
+single_model=nn.Sequential(linear,sigmoid).to(device)
+model=nn.Sequential(
+                    nn.Linear(2,10,bias=True),
+                    sigmoid,
+                    nn.Linear(10,10,bias=True),
+                    sigmoid,
+                    nn.Linear(10,10,bias=True),
+                    sigmoid,
+                    nn.Linear(10,1,bias=True),
+                    sigmoid,
+                    ).to(device)
 
 #loss function to use binary classification
 criterion=nn.BCELoss().to(device)
 #gredient descent(lr: how much move the differential value)
 optimizer=torch.optim.SGD(model.parameters(),lr=1)
 
-#train model
+#train single layer model
 epoch=10001
+for step in range(epoch):
+    optimizer.zero_grad()
+    hypothesis=single_model(X)
+
+    #cost function
+    cost=criterion(hypothesis,Y)
+    cost.backward()
+    optimizer.step()
+    
+    #print cost
+    if step %100==0:
+        print(step,cost.item())
+
+#single layer model test
+with torch.no_grad():
+    hypothesis=model(X)
+    predicted=(hypothesis>0.5).float()
+    accuracy=(predicted==Y).float().mean()
+    print('Hypothesis: ', hypothesis.detach().cpu().numpy())
+    print('Predicted: ', predicted.detach().cpu().numpy())
+    print('Real value: ', Y.cpu().numpy())
+    print('Accuracy: ', accuracy.item())
+
+#train multi Layer model
 for step in range(epoch):
     optimizer.zero_grad()
     hypothesis=model(X)
@@ -37,7 +71,7 @@ for step in range(epoch):
     if step %100==0:
         print(step,cost.item())
 
-#model test
+#multi layer model test
 with torch.no_grad():
     hypothesis=model(X)
     predicted=(hypothesis>0.5).float()
