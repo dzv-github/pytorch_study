57803e2dcdb9bd9d354f895f2d2efb946ef5ddda
dzv-github <85002532+dzv-github@users.noreply.github.com>
2023-12-09 14:05:57 +0900

    Update softmax(iris_ditection).py

--- softmax(iris_ditection).py
+++ softmax(iris_ditection).py
@@ -10,23 +10,29 @@ onehot=OneHotEncoder()
 device='cuda' if torch.cuda.is_available() else 'cpu'
 iris = datasets.load_iris()
 
+#iris dataset load
 data1 = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
                      columns= iris['feature_names'] + ['target'])
 
+#iris dataset to dataframe
 temp_data=pd.DataFrame()
 temp_data['ordinal']=data1['target']
+
+#one hot on result
 temp_data=pd.DataFrame(onehot.fit_transform(temp_data).toarray())
-
 d=pd.get_dummies(temp_data)
-X=torch.FloatTensor(data1.iloc[:, 0:4].values).to(device)
 
+#set X,y
+X=torch.FloatTensor(data1.iloc[:, 0:4].values).to(device)
 y=torch.FloatTensor(d.iloc[:, 0:4].values).to(device)
 
-softmax=nn.Softmax()
+#set loss func
 criterion=nn.CrossEntropyLoss().to(device)
+
+#epoch parameter
 epoch=10001
 
-#############################################################
+#set model architecture
 model=nn.Sequential(
     nn.Linear(4,20,bias=True),
     nn.ReLU(),
@@ -43,11 +49,13 @@ model=nn.Sequential(
     nn.Linear(20,20,bias=True),
     nn.ReLU(),
     nn.Linear(20,3,bias=True),
-    softmax,
+    nn.Softmax(),
 ).to(device)
-#############################################################
 
+#set optimizer
 optimizer=torch.optim.SGD(model.parameters(),lr=0.01)
+
+#learning
 for step in range(epoch):
     optimizer.zero_grad()
     hypothesis=model(X)
@@ -59,7 +67,7 @@ for step in range(epoch):
     if step%1000==0:
         print(step,cost.item())
 
-
+#result output
 with torch.no_grad():
     hypothesis=model(X)
     predicted=(torch.argmax(hypothesis,dim=1)).float()
