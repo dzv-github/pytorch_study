cc6e16234de838c3f6ad6e2d12bbceb99ab4a804
dzv-github <85002532+dzv-github@users.noreply.github.com>
2024-10-24 15:15:59 +0900

    Update back_propagation.cpp

--- xor_problem/back_propagation(unfinished).cpp
+++ xor_problem/back_propagation(unfinished).cpp
@@ -21,7 +21,7 @@ struct weight_location
 		if (st_layer != var.st_layer) {
 			return st_layer < var.st_layer;
 		}
-		else if(st_column!=var.st_column){
+		else if (st_column != var.st_column) {
 			return st_column < var.st_column;
 		}
 		else if (ed_layer != var.ed_layer) {
@@ -46,6 +46,20 @@ struct node_location {
 		}
 	}
 };
+struct bias_location {
+	int prior_layer;
+	int next_layer;
+
+	bool operator <(const bias_location& var) const
+	{
+		if (prior_layer != var.prior_layer) {
+			return prior_layer < var.prior_layer;
+		}
+		else {
+			return next_layer < var.next_layer;
+		}
+	}
+};
 struct node_values {
 	long double net;
 	long double out;
@@ -58,10 +72,11 @@ long double max(long double a, long double b) {
 
 random_device rd;
 mt19937 gen(rd());
-uniform_real_distribution<double>dis(0,1);
+uniform_real_distribution<double>dis(0, 1);
 
 map<weight_location, long double>weight;
-map<node_location, long double>bias;
+map<weight_location, long double>new_weight;
+map<bias_location, long double>bias;
 map<node_location, vector<node_location> >prior_nodes;
 map<node_location, vector<node_location> >next_nodes;
 map<node_location, node_values>node; //pair<net,out>
@@ -69,15 +84,17 @@ map<node_location, long double>delta;
 vector<int>network_size;
 vector<string>act_func;
 vector<bool>bias_usage;
+long double temp_weight=0.15;
 
-int epoch=1000;
-int input_size = 4;
-long double learning_rate = 0.1;
+int epoch = 10000;
+int input_size = 1;
+long double learning_rate = 0.5;
 
-long double input[5][3] = {{0, 0}, {1, 0}, {0, 1}, {1, 1}};
-long double correct_output[5] = { 0,1,1,0 };
-long double output[5];
-
+long double input[5][3] = { {0.05, 0.1} };
+long double correct_output[5][3] = {{0.01,0.99}};
+long double output[5][3];
+map<int,long double> error;
+long double total_error;
 
 void connect() {
 	for (int i = 0;i < network_size.size();i++) {
@@ -86,8 +103,10 @@ void connect() {
 			if (i != network_size.size() - 1) {
 				for (int k = 0;k < network_size[i + 1];k++) {
 					next_nodes[{i, j}].push_back({ i + 1,k });
-					weight[{i, j, i + 1, k}] = dis(gen);
+					weight[{i, j, i + 1, k}] = temp_weight;
+					temp_weight += 0.05;
 				}
+				
 			}
 
 			if (i != 0) {
@@ -96,7 +115,11 @@ void connect() {
 				}
 			}
 		}
+
+		bias[{i, i + 1}] = temp_weight;
+		temp_weight += 0.05;
 	}
+
 	return;
 }
 
@@ -110,18 +133,15 @@ long double sigmoid(long double x) {
 }
 
 
-
-long double MSELoss(long double )
-
-void perceptron(int layer, int column,string activative_func,bool set_bias) {
+void perceptron(int layer, int column, string activative_func, bool set_bias) {
 	long double result = 0;
-	if (set_bias == true)result = bias[{layer, column}];
+	if (set_bias == true)result = bias[{layer-1, layer}];
 
 	for (int i = 0;i < prior_nodes[{layer, column}].size();i++) {
 		result += weight[{ prior_nodes[{layer, column}][i].layer, prior_nodes[{layer, column}][i].column, layer, column}] *
 			node[{prior_nodes[{layer, column}][i].layer, prior_nodes[{layer, column}][i].column}].out;
-	}
 
+	}
 	if (activative_func == "sigmoid")
 		node[{layer, column}] = { result,sigmoid(result) };
 	else if (activative_func == "ReLU")
@@ -140,13 +160,10 @@ void sequential() {
 	linear(2, true);
 	act_func.push_back("sigmoid");
 
-	linear(10, true);
+	linear(2, true);
 	act_func.push_back("sigmoid");
 
-	linear(10, true);
-	act_func.push_back("sigmoid");
-
-	linear(1, true);
+	linear(2,true);
 	act_func.push_back("sigmoid");
 
 	connect();
@@ -165,24 +182,78 @@ void forward_Pass(int n) {
 	}
 	return;
 }
+map<weight_location, long double> temp1, temp2, temp3, temp4;
+int size_of_network;
+void backward(string loss_fuction,int input_num) {
 
-void backward(string loss_fuction) {
-	
+	size_of_network = network_size.size()-1;
+	for (int j = 0;j < network_size[size_of_network];j++) {
+		for (int k = 0;k < prior_nodes[{size_of_network,j}].size();k++) {
+			temp1[{prior_nodes[{size_of_network, j}][k].layer, prior_nodes[{size_of_network, j}][k].column, size_of_network, j}] = 
+				-(correct_output[input_num][j] - node[{size_of_network, j}].out);
+
+			temp2[{prior_nodes[{size_of_network, j}][k].layer, prior_nodes[{size_of_network, j}][k].column, size_of_network, j}] =
+				node[{size_of_network, j}].out* (1 - node[{size_of_network, j}].out);
+
+			temp3[{prior_nodes[{size_of_network, j}][k].layer, prior_nodes[{size_of_network, j}][k].column, size_of_network, j}] =
+				node[{prior_nodes[{size_of_network, j}][k].layer, prior_nodes[{size_of_network, j}][k].column}].out;
+
+			temp4[{prior_nodes[{size_of_network, j}][k].layer, prior_nodes[{size_of_network, j}][k].column, size_of_network, j}] =
+				temp1[{prior_nodes[{size_of_network, j}][k].layer, prior_nodes[{size_of_network, j}][k].column, size_of_network, j}] * 
+				temp2[{prior_nodes[{size_of_network, j}][k].layer, prior_nodes[{size_of_network, j}][k].column, size_of_network, j}] *
+				temp3[{prior_nodes[{size_of_network, j}][k].layer, prior_nodes[{size_of_network, j}][k].column, size_of_network, j}];
+
+			new_weight[{prior_nodes[{size_of_network, j}][k].layer, prior_nodes[{size_of_network, j}][k].column, size_of_network, j}] =
+				weight[{prior_nodes[{size_of_network, j}][k].layer, prior_nodes[{size_of_network, j}][k].column, size_of_network, j}] - 
+				(learning_rate * temp4[{prior_nodes[{size_of_network, j}][k].layer, prior_nodes[{size_of_network, j}][k].column, size_of_network, j}]);
+		}
+	}
+
+	for (int i = size_of_network - 1;i >= 0;i--) {
+		for (int j = 0;j < network_size[i];j++) {
+			for (int k = 0;k < prior_nodes[{i, j}].size();k++) {
+				temp1[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column, i, j}] = 0;
+				for (int n = 0;n < next_nodes[{i, j}].size();n++) {
+					temp1[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column, i, j}] +=
+						(temp1[{i, j, next_nodes[{i, j}][n].layer, next_nodes[{i, j}][n].column}] *
+							temp2[{i, j, next_nodes[{i, j}][n].layer, next_nodes[{i, j}][n].column}])*
+						weight[{i, j, next_nodes[{i, j}][n].layer, next_nodes[{i, j}][n].column}];
+
+				}
+
+				temp2[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column, i, j}] = node[{i, j}].out* (1 - node[{i, j}].out);
+				temp3[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column, i, j}] = node[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column}].out;
+				temp4[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column, i, j}] =
+					temp1[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column, i, j}] *
+					temp2[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column, i, j}] *
+					temp3[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column, i, j}];
+
+				new_weight[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column, i, j}] = weight[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column, i, j}] -
+					learning_rate * temp4[{prior_nodes[{i, j}][k].layer, prior_nodes[{i, j}][k].column, i, j}];
+			}
+		}
+	}
+	weight = new_weight;
+	return;
 }
 int main()
 {
 	sequential();
 
 	for (int step = 0;step < epoch;step++) {
+		total_error = 0;
 		for (int i = 0;i < input_size;i++) {
 			forward_Pass(i);
-			output[i] = node[{input_size-1, 0}].out;
+			output[i][0] = node[{size_of_network, 0}].out;
+			output[i][1] = node[{size_of_network, 1}].out;
+			error[0] = pow((output[i][0] - correct_output[i][0]), 2) / 2;
+			error[1] = pow((output[i][1] - correct_output[i][1]), 2) / 2;
+			cout << output[i][0] << " " << output[i][1]<<endl;
+			total_error = pow((output[i][0] - correct_output[i][0]), 2) / 2 + pow((output[i][1] - correct_output[i][1]), 2) / 2;
+			cout << total_error << endl;
+			backward("MSELoss",i);
+
 		}
-		
-		backward("MSELoss");
-		if(step%100==0)
-			cout<<output[0]<<" "<<output[1]<<" "<<output[2]<<" "<<output[3]<<endl;
 	}
-	cout << output[3] << " " << correct_output[2];
 	return 0;
 }
